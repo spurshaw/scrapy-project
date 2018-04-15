f22f4f2ab2d72f374e8e37b322ab2f26474e1681
thinkxiao <38377376+thinkxiao@users.noreply.github.com>
2018-04-15 15:21:43 +0800

    Add files via upload

---
+++ lianjia/log/myLogging.py
@@ -0,0 +1,17 @@
+import logging
+import datetime
+
+def logging_handle(loggername):
+    '''
+    用于处理日志文件
+    :param loggername: 日志名
+    :param filename: 日志文件名
+    :return: 返回日志对象
+    '''
+    logger = logging.getLogger(loggername)
+    formatter = logging.Formatter('%(name)s %(asctime)s %(lineno)d %(levelname)-8s:%(message)s')
+    file_handler = logging.FileHandler('../log/%s' % datetime.date.today(), encoding='utf8')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.INFO)
+    return logger
